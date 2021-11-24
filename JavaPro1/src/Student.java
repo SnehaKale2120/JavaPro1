897d78a3dded1858ea0e872e21e00831af5a837f
skale22 <skale22@host.docker.internal>
2021-11-24 10:44:35 +0530

    First Commit

---
+++ JavaPro1/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+private int roll;
+private String sname;
+public int getRoll() {
+	return roll;
+}
+public void setRoll(int roll) {
+	this.roll = roll;
+}
+public String getSname() {
+	return sname;
+}
+public void setSname(String sname) {
+	this.sname = sname;
+}
+
+}
